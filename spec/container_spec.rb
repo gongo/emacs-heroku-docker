be4531b57e1704126f903efd18a2a63f167fbefb
Wataru MIYAGUNI <gonngo@gmail.com>
2015-11-15 00:16:46 +0900

    Add tests

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -7,3 +7,15 @@ end
 describe file('/app/cask/bin/cask') do
   it { should exist }
 end
+
+describe file('/app/user') do
+  it { should be_directory }
+end
+
+describe file('/app/user/.cask') do
+  it { should be_directory }
+end
+
+describe file('/app/.profile.d/path.sh') do
+  it { should exist }
+end
